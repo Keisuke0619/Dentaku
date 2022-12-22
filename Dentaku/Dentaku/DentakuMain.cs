e80d8267a8821205950a91bfdd848d82b7db6f82
SimizuKeisuke <>
2022-12-23 06:00:24 +0900

    見た目だけ

---
+++ Dentaku/Dentaku/DentakuMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentaku
+{
+	enum EOperator
+	{
+		Add,
+		Subtract,
+		Multiply,
+		Divide,
+		Equal,
+		Dot,
+	}
+	internal class DentakuMain
+	{
+		public static DentakuMain ins = new DentakuMain();
+
+		private float frontNum;	// 表に出ている数字
+		private float backNum;  // 裏で保持している数字
+		private bool isFloat;	// 小数か？
+		DentakuMain() { }
+		
+		public void PushNum(int num)
+		{
+			this.frontNum *= 10;
+			this.frontNum += num;
+		}
+
+		public void PressOperator(EOperator op)
+		{
+			
+		}
+
+	}
+}
---
+++ Dentaku/Dentaku/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dentaku
+{
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click_Dot(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Divde(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Multiply(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Subtract(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Add(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Equal(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_0(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_1(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_2(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_3(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_4(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_5(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_6(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_7(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_8(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_9(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_0(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void Button_Click_Num_00(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+
+		}
+	}
+}
